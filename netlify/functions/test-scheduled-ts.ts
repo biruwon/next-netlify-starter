a00f105097bf0156b7e6f20882da93d626d05607
Antonio Jesús <antonio.rodriguez@netlify.com>
2022-04-21 15:14:05 +0200

    Added more functions

--- netlify/functions/test-scheduled-ts.ts
+++ netlify/functions/test-scheduled-ts.ts
@@ -1,6 +1,6 @@
 import { schedule } from "@netlify/functions";
 
-export const handler = schedule("* * * * *", async () => {
+export const handler = schedule("0 1 * * *", async () => {
     console.log("Me every minute!")
 
     return {
